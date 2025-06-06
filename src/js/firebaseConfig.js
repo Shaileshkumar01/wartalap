02c10a49514c5738ac8c9b8f8c78fbec5eac22c2
Shaileshkumar01 <153973933+Shaileshkumar01@users.noreply.github.com>
2025-06-06 17:56:55 +0530

    Update firebaseConfig.js

--- src/js/firebaseConfig.js
+++ src/js/firebaseConfig.js
@@ -4,16 +4,16 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
-// Replace this config with your actual Firebase project config
+// ✅ Correct config with fixed storageBucket
 const firebaseConfig = {
-    apiKey: "AIzaSyD14hcTY9kDBJj2L39ff2Cm_GlculEtRxc",
-    authDomain: "wartalap-c7311.firebaseapp.com",
-    projectId: "wartalap-c7311",
-    storageBucket: "wartalap-c7311.firebasestorage.app",
-    messagingSenderId: "397454314037",
-    appId: "1:397454314037:web:59206437e766e2e82b7f7c",
-    measurementId: "G-DM3HDBPDZW"
-  };
+  apiKey: "AIzaSyD14hcTY9kDBJj2L39ff2Cm_GlculEtRxc",
+  authDomain: "wartalap-c7311.firebaseapp.com",
+  projectId: "wartalap-c7311",
+  storageBucket: "wartalap-c7311.appspot.com", // <-- fixed
+  messagingSenderId: "397454314037",
+  appId: "1:397454314037:web:59206437e766e2e82b7f7c",
+  measurementId: "G-DM3HDBPDZW"
+};
 
 const app = initializeApp(firebaseConfig);
 
